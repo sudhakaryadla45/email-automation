ee8a3085eaf3cf6f59e74daa7abb8d3198955a07
sudhakaryadla45 <sudhakaryadla453@gmail.com>
2024-10-23 03:00:13 +0530

    index.js

--- index.js
+++ index.js
@@ -69,13 +69,7 @@ const getAnniversaryData = async () => {
 
 
 
-function Person(Name, DOB, Email, Marr, Anv) {
-    this.name = Name;
-    this.dob = DOB;
-    this.email = Email;
-    this.marriageDate = Marr;
-    this.anniversary = Anv;
-}
+
 
 
 
